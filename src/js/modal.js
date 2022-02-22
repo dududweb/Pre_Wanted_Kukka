2918a611f0e045cead112fadde1c58ce6aaf94ff
dnjstd <dnjstd@gmail.com>
2022-02-22 21:55:36 +0900

    Fix: modal 임시 비노출

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,14 @@
-$("html, body").css({"overflow":"hidden", "height":"100%"});
-$(".modalWrapper").bind("touchmove", function(e) {
-	e.preventDefault();
+$('html, body').css({ overflow: 'hidden', height: '100%' });
+$('.modalWrapper').bind('touchmove', function (e) {
+  e.preventDefault();
 });
-$(".modalWrapper .inner").bind("touchmove", function(e) {
-	e.stopPropagation();
+$('.modalWrapper .inner').bind('touchmove', function (e) {
+  e.stopPropagation();
 });
 
+$('.modalContainer').click(function () {
+  $('.modalContainer').css({ display: 'none' });
 
-$(".modalContainer").click(function(){
-  $(".modalContainer").css({"display" : "none"});
-
-  $("html, body").css({"overflow":"auto", "height":"auto"});
+  $('html, body').css({ overflow: 'auto', height: 'auto' });
   $('.modalWrapper').unbind('touchmove');
-})
+});
