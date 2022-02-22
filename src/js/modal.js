a1cfb4f4e617fe5520426f2a5284a23ba22022e3
dnjstd <dnjstd@gmail.com>
2022-02-22 21:23:17 +0900

    Add: modal 팝업 시 body 스크롤 고정

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,15 @@
+$("html, body").css({"overflow":"hidden", "height":"100%"});
+$(".modalWrapper").bind("touchmove", function(e) {
+	e.preventDefault();
+});
+$(".modalWrapper .inner").bind("touchmove", function(e) {
+	e.stopPropagation();
+});
+
+
 $(".modalContainer").click(function(){
   $(".modalContainer").css({"display" : "none"});
+
+  $("html, body").css({"overflow":"auto", "height":"auto"});
+  $('.modalWrapper').unbind('touchmove');
 })
-
-
-
